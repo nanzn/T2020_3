5b37e8dc9c8e52046d26985983839750f452b5a0
Reynold Chia <CH0001LD@e.ntu.edu.sg>
2020-01-07 15:12:10 +0800

    Added components

--- app-dbs-seed/src/app/app-routing.module.ts
+++ app-dbs-seed/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { TagPageComponent } from './tag-page/tag-page.component';
+import { LogoutComponent } from './logout/logout.component';
+import { PromotionsComponent } from './promotions/promotions.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'tagpage', component: TagPageComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'promotions', component: PromotionsComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgetpassword', component: ForgetpasswordComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
